036b80b2a39f3383e625776b457266af40fc2b5c
psukow <psukow@unifocus.com>
2016-09-13 09:39:22 -0600

    fixed missing import

--- generators/app/templates/src/test/resources/GebConfig.groovy
+++ generators/app/templates/src/test/resources/GebConfig.groovy
@@ -1,4 +1,5 @@
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.phantomjs.PhantomJSDriver
 
